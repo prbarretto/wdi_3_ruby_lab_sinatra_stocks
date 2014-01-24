b61005d662fed397df57923c0750ea2e78b8ebbc
Praggya Rustagi Barretto <praggya@gmail.com>
2014-01-24 14:04:51 -0500

    sinatra stocks

--- main.rb
+++ main.rb
@@ -1,8 +1,11 @@
 require 'pry'
 require 'sinatra'
 require 'sinatra/reloader' if development?
-require 'yahoofinance'
+require 'yahoo_finance'
 
-get '/' do
+get '/quote/:ticker' do
+	@ticker = params[:ticker]
+	@result = YahooFinance.quotes([@ticker], [:last_trade_price])[0]
   erb :quote
 end
+
